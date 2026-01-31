0cd714d99dfab8cf4a73d9900d8e7ed7cea1a060
rjnunesdev <rjnunes.dev@gmail.com>
2026-01-31 17:04:17 -0300

    calculando média

--- Principal.java
+++ Principal.java
@@ -1,9 +1,11 @@
 import java.util.Scanner;
 
 public class Principal {
-   //enunciado: escreva um programa que peça ao usuário o seu  nome e 3 notas com uma casa decimal,
-   // que devem ser inseridas individualmente e exiba a média aritimética delas.
-
+    /* enunciado: escreva um programa que peça ao usuário 3 notas
+    com uma casa decimal, que devem ser inseridas individualmente, exiba a média
+    aritimética delas e exiba se APROVADO se a média for maior a 7,
+    caso contrario exiba reprovado
+     */
     public static void main (String[] args){
         Scanner entrada= new Scanner(System.in);
         System.out.println(" Nota 1: ");
@@ -14,6 +16,14 @@ public class Principal {
         double nota3 = entrada.nextDouble();
         double media = (nota1 + nota2 + nota3)/3;
         System.out.println("a média das notas é: " + media);
+        if (media >7 ){
+            System.out.println(" Aprovado ");
+        } else if(media >=5){
+            System.out.println(" Em recuperação!!!!! ");
+        }
+        else {
+            System.out.println(" REPROVADO ");
+        }
 
 
 
