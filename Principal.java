3a0db036fb41fbdf6c8dfbb2bf66a46674f676bc
rjnunesdev <rjnunes.dev@gmail.com>
2026-02-23 12:20:41 -0300

    calculando média dinamicamente com uso do for

--- Principal.java
+++ Principal.java
@@ -8,13 +8,19 @@ public class Principal {
      */
     public static void main (String[] args){
         Scanner entrada= new Scanner(System.in);
-        System.out.println(" Nota 1: ");
-        double nota1 = entrada.nextDouble();
-        System.out.println(" Nota 2: ");
-        double nota2 = entrada.nextDouble();
-        System.out.println(" Nota 3: ");
-        double nota3 = entrada.nextDouble();
-        double media = (nota1 + nota2 + nota3)/3;
+        System.out.println("Inserir quantidade de notas");
+        int qtdNotas = entrada.nextInt();
+        double[] notas= new double[qtdNotas];
+
+        double somatorio =0;
+        for(int posicao = 0; posicao < qtdNotas; posicao++){
+            int ordemNota = posicao + 1;
+            System.out.println("Insira nota "+ordemNota);
+            notas[posicao] =entrada.nextDouble();
+            somatorio = somatorio + notas[posicao];
+        }
+
+        double media = (somatorio)/ qtdNotas;
         System.out.println("a média das notas é: " + media);
         if (media >7 ){
             System.out.println(" Aprovado ");
